5edfb50eb7dbfbc382c8d3408edade300fd842d4
rolikpeter <rolikpeti5@gmail.com>
2025-03-16 21:48:22 +0100

    gen.cpp

--- launch/launch_example1.launch.py
+++ launch/launch_example1.launch.py
@@ -1,34 +1,48 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <cmath>  
 
-def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            namespace='turtlesim2',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            executable='mimic',
-            name='mimic',
-            remappings=[
-                ('/input/pose', '/turtlesim1/turtle1/pose'),
-                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-            ]
-        ),
-        # Node(
-        #     package='ros2_cpp_template',
-        #     executable='simple_sub_node',
-        #     output='screen',
-        # ),
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32.hpp"
 
-        # first comment
-    ])
+using namespace std::chrono_literals;
+
+class GenTanAndRand : public rclcpp::Node
+{
+public:
+    GenTanAndRand(): Node("gen_node"), count_(0)
+    {
+        RCLCPP_INFO(this->get_logger(), "Generating tangent and random numbers");
+        pub_tan_ = this->create_publisher<std_msgs::msg::Float32>("tangent", 10);
+        pub_rand_ = this->create_publisher<std_msgs::msg::Float32>("rand", 10);
+        timer_ = this->create_wall_timer(50ms, std::bind(&GenTanAndRand::timer_callback, this));
+    }
+
+private:
+    void timer_callback()
+    {
+        auto message_tan = std_msgs::msg::Float32();
+        auto message_rand = std_msgs::msg::Float32();
+        
+        message_tan.data = tan(count_++ / 50.0) * 100;  // A tangens értéket 100-zal szorozzuk
+        
+        message_rand.data = rand() % 5; 
+        
+        pub_tan_->publish(message_tan);
+        pub_rand_->publish(message_rand);
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_tan_, pub_rand_;
+    size_t count_;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<GenTanAndRand>());
+    rclcpp::shutdown();
+    return 0;
+}
